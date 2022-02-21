423a5371fbd20fb9fa1707600455a04c2c490ebd
astrakore <teresa.carrasco.sauri@gmail.com>
2022-02-21 10:18:29 +0100

    Views creadas falta crear controllers

---
+++ views/PeliculasRouter.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+
+const PeliculasController = require('../controllers/PeliculasController');
+
+//CRUD RESTful
+
+//Leer todas las peliculas
+router.get('/', PeliculasController.traePeliculas);
+//http://localhost:3000/peliculas
+
+//Registro de una peli nueva
+router.post('/', PeliculasController.registraPelicula);
+//http://localhost:3000/peliculas
+
+//Búsqueda de películas por título
+router.get('/titulo', PeliculasController.peliculasTitulo);
+
+//Búsqueda de novedades
+router.get('/novedades', PeliculasController.traeNovedades);
+
+
+module.exports = router;
---
+++ views/UsuarioRouter.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const UsuarioController = require('../controllers/UsuarioController');
+
+//CRUD RESTful
+
+//Leer todos los usuarios
+router.get('/', UsuarioController.traeUsuarios);
+//http://localhost:3000/usuarios
+
+
+router.get('/email/:email', UsuarioController.traerUsuarioEmail);
+router.get('/:id', UsuarioController.traerUsuarioId);
+
+//Registro
+router.post('/', UsuarioController.registraUsuario);
+//http://localhost:3000/usuarios
+
+//Modificar datos de un Usuario
+router.put('/:id', UsuarioController.updateProfile);
+
+//Borramos a todos los usuarios
+router.delete('/', UsuarioController.deleteAll);
+
+//Borramos a todos los usuarios
+router.delete('/:id', UsuarioController.deleteById);
+
+//Login
+router.post('/login', UsuarioController.logUsuario);
+//https://localhost:3000/usuarios/login
+
+module.exports = router;
